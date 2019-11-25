c91defa4380a7390659e6ef4584e652b591fd30f
Matej Vasilevski <matej.vasilevski@seznam.cz>
2019-11-25 23:11:15 +0100

    Make camera great again

--- semestralka.py
+++ semestralka.py
@@ -3,11 +3,13 @@ import pybullet_data
 import time
 import numpy as np
 
+
 def print_joint_info(robot):
     print(f"joint info for robot {robot}")
     num = p.getNumJoints(robot)
     for i in range(num):
         print(p.getJointInfo(robot, i))
+
 
 def print_joint_states(robot):
     print(f"joint states for robot {robot}")
@@ -16,11 +18,39 @@ def print_joint_states(robot):
         print(p.getJointState(robot, i))
     # print(p.getJointStates(robot, range(num)))
 
+
 def print_link_states(robot):
     print(f"link states for robot {robot}")
     num = p.getNumJoints(robot)
     for i in range(num):
         print(p.getLinkState(robot, i, computeForwardKinematics=True))
+
+
+def calc_projection_matrix():
+    fov, aspect, nearplane, farplane = 60, 1.0, 0.03, 10
+    projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, nearplane, farplane)
+    return projection_matrix
+
+
+def calc_view_matrix(position, orientation):
+    rot_matrix = p.getMatrixFromQuaternion(orientation)
+    rot_matrix = np.array(rot_matrix).reshape(3, 3)
+    init_camera_vector = (0, 0, 1)
+    init_up_vector = (0, 1, 0)
+    camvec = rot_matrix.dot(init_camera_vector)
+    upvec = rot_matrix.dot(init_up_vector)
+    view_matrix = p.computeViewMatrix(position, position + 0.1 * camvec, upvec)
+    return view_matrix, camvec
+
+
+projection_matrix = calc_projection_matrix()
+
+
+def img_wrapper(robot):
+    pos, orn, _, _, _, _ = p.getLinkState(robot, p.getNumJoints(robot) - 1, computeForwardKinematics=True)
+    view_matrix, camvec = calc_view_matrix(pos, orn)
+    return p.getCameraImage(128, 128, view_matrix, projection_matrix), pos, camvec
+
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -35,44 +65,75 @@ p.setRealTimeSimulation(useRealTimeSimulation)
 print_joint_info(kukaId)
 print_joint_states(kukaId)
 print_link_states(kukaId)
+
 numjoints = p.getNumJoints(kukaId)
 positions = [0] * numjoints
-p.setJointMotorControlArray(kukaId, range(numjoints), p.POSITION_CONTROL, targetPositions=positions)
-
-fov, aspect, nearplane, farplane = 60, 1.0, 0.01, 100
-projection_matrix = p.computeProjectionMatrixFOV(fov, aspect, nearplane, farplane)
-
-ballId = p.loadURDF("sphere2red.urdf", [2, 2, 0])
-time.sleep(5)
-
-# for i in range(100):
-#   joint = np.random.randint(numjoints)
-#   newval = np.random.uniform(-3.14, 3.14)
-#   positions[joint] = newval
-#   print(f"setting {joint=} to {newval=}")
-#   p.setJointMotorControlArray(kukaId, range(numjoints), p.POSITION_CONTROL, targetPositions=positions)
-#   print_joint_states(kukaId)
-  # for _ in range(100):
-  #   p.stepSimulation()
-  # vykradeno z https://github.com/bulletphysics/bullet3/issues/1616
-  # pos, orn, _, _, _, _ = p.getLinkState(kukaId, numjoints - 1)
-  # rot_matrix = p.getMatrixFromQuaternion(orn)
-  # rot_matrix = np.array(rot_matrix).reshape(3, 3)
-  # camvec = [0, 1, 0]  # kamera kouka smerem osy y
-  # camera_vector = rot_matrix.dot(camvec)
-  # zvec = [0, 0, 1] # upvector kamery je smerem osy z
-  # camera_upvector = rot_matrix.dot(zvec)
-  # view_matrix = p.computeViewMatrix(pos, pos + 0.1*camera_vector, camera_upvector)
-  #
-  # img = p.getCameraImage(128, 128, view_matrix, projection_matrix)
-  #
-  # print(f"------------- iteration {i} over")
-  # time.sleep(2)
 
 
+def test_camera():
+    # zajimavy pozice na testovani:
+    # [1.1731611377222513, -2.290785729075651, -1.2009673967971037, 0, -1.509407331996121, 2.6989293575021738, 0]
 
-# pos, orn, _, _, _, _ = p.getLinkState(kukaId, numjoints-1, computeForwardKinematics=True)
-# print(pos, orn)
-# print("DONE")
-# time.sleep(5)
-# print_joint_states(kukaId)
+    positions = [0.186, 0.306, 0.847, -0.745, -0.131, 0.418, 0.036]  # pohled zblizka s kulickou 0.5 0.5 1
+    ballId = p.loadURDF("sphere2red.urdf", [0.5, 0.5, 1], globalScaling=0.1, useFixedBase=True)
+    ballId2 = p.loadURDF("sphere2red.urdf", [0.6, 0.5, 1.1], globalScaling=0.1, useFixedBase=True)
+    p.setJointMotorControlArray(kukaId, range(numjoints), p.POSITION_CONTROL, targetPositions=positions)
+    for i in range(500):
+        p.stepSimulation()
+    img, pos, camvec = img_wrapper(kukaId)
+    time.sleep(2)
+    newpos = pos + camvec
+    print(f"newpos is {newpos}")
+    p.resetBasePositionAndOrientation(ballId2, (1, 1, 0), p.getQuaternionFromEuler((0, 0, 0)))
+    p.stepSimulation()
+    img = img_wrapper(kukaId)
+    time.sleep(173)
+
+
+def test_camera_do_dalky():
+    positions = [0.664, -0.064, -0.702, -0.64, 0.218, 2.093, -0.33]  # pohled zdalky na kulicku 0.5 0 0
+    ballId = p.loadURDF("sphere2red.urdf", [0.5, 0, 0], globalScaling=0.1, useFixedBase=True)
+    ballId2 = p.loadURDF("sphere2red.urdf", [1, 0, 0], globalScaling=0.1, useFixedBase=True)
+    p.setJointMotorControlArray(kukaId, range(numjoints), p.POSITION_CONTROL, targetPositions=positions)
+    for i in range(500):
+        p.stepSimulation()
+    # pos, orn, _, _, _, _ = p.getLinkState(kukaId, numjoints - 1)
+    # view_matrix, camvec = calc_view_matrix(pos, orn)
+    # img = p.getCameraImage(128, 128, view_matrix, projection_matrix)
+    img, pos, camvec = img_wrapper(kukaId)
+    time.sleep(2)
+    newpos = pos + camvec
+    print(f"newpos is {newpos}")
+    p.resetBasePositionAndOrientation(ballId2, (1, 1, 0), p.getQuaternionFromEuler((0, 0, 0)))
+    p.stepSimulation()
+    img = img_wrapper(kukaId)
+    time.sleep(173)
+
+
+def freestyle():
+    ballId = p.loadURDF("sphere2red.urdf", [0.5, 0, 0], globalScaling=0.1, useFixedBase=True)
+    for i in range(100):
+        joint = np.random.randint(numjoints - 1)
+        newval = np.random.uniform(-3.14, 3.14)
+        positions[joint] = newval
+        print(f"setting {joint=} to {newval=}")
+        p.setJointMotorControlArray(kukaId, range(numjoints), p.POSITION_CONTROL, targetPositions=positions)
+        #   print_joint_states(kukaId)
+        for _ in range(100):
+            p.stepSimulation()
+        # vykradeno z https://github.com/bulletphysics/bullet3/issues/1616
+        # pos, orn, _, _, _, _ = p.getLinkState(kukaId, numjoints - 1)
+        # view_matrix, camera_vector = calc_view_matrix(pos, orn)
+        # img = p.getCameraImage(192, 128, view_matrix, projection_matrix)
+        img, pos, camera_vector = img_wrapper(kukaId)
+        p.resetBasePositionAndOrientation(ballId, pos + 0.2 * camera_vector, p.getQuaternionFromEuler((0, 0, 0)))
+        img, pos, camera_vector = img_wrapper(kukaId)
+
+        print(f"------------- iteration {i} over")
+        print(f"positions are {positions}")
+        time.sleep(10)
+
+
+# test_camera()
+# test_camera_do_dalky()
+freestyle()
